abcfcb73bbfe45640a939a51353cc29a832ad912
Shiv Chro <shivchro@gmail.com>
2026-05-04 08:19:19 -0400

    feat: merge CoursesPage+SubjectsPage into unified subject/unit screen (Prompt 04)

--- frontend/web/src/App.jsx
+++ frontend/web/src/App.jsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import { isLoggedIn } from './api/auth'
-import LoginPage    from './pages/LoginPage'
-import HomePage     from './pages/HomePage'
-import LearnPage    from './pages/LearnPage'
-import PracticePage from './pages/PracticePage'
-import ProgressPage from './pages/ProgressPage'
-import NotFoundPage from './pages/NotFoundPage'
+import { isLoggedIn }    from './api/auth'
 
-function PrivateRoute({ children }) {
+import LoginPage         from './pages/LoginPage'
+import HomePage          from './pages/HomePage'
+import CoursesPage       from './pages/CoursesPage'
+import SubjectsPage      from './pages/SubjectsPage'
+import TopicListPage     from './pages/TopicListPage'
+import TopicDetailPage   from './pages/TopicDetailPage'
+import LearnPage         from './pages/LearnPage'
+import PracticePage      from './pages/PracticePage'
+import ProgressPage      from './pages/ProgressPage'
+
+function Guard({ children }) {
   return isLoggedIn() ? children : <Navigate to="/login" replace />
 }
 
@@ -15,20 +19,50 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+
         <Route path="/login" element={<LoginPage />} />
+
         <Route path="/" element={
-          <PrivateRoute><HomePage /></PrivateRoute>
+          <Guard><HomePage /></Guard>
         } />
-        <Route path="/learn/:chapterId" element={
-          <PrivateRoute><LearnPage /></PrivateRoute>
+
+        <Route path="/courses" element={
+          <Guard><CoursesPage /></Guard>
         } />
-        <Route path="/practice/:chapterId" element={
-          <PrivateRoute><PracticePage /></PrivateRoute>
+        <Route path="/courses/:classId" element={
+          <Guard><SubjectsPage /></Guard>
         } />
+        <Route path="/courses/:classId/:subjectSlug" element={
+          <Guard><TopicListPage /></Guard>
+        } />
+        <Route path="/courses/:classId/:subjectSlug/:chapterSlug" element={
+          <Guard><TopicDetailPage /></Guard>
+        } />
+
+        <Route path="/class/:id"   element={<Navigate to="/courses" replace />} />
+        <Route path="/subject/:id" element={<Navigate to="/courses" replace />} />
+
+        <Route path="/chapters" element={<Navigate to="/courses" replace />} />
+        <Route path="/chapters/:subjectSlug" element={
+          <Guard><TopicListPage /></Guard>
+        } />
+        <Route path="/chapters/:subjectSlug/:chapterSlug" element={
+          <Guard><TopicDetailPage /></Guard>
+        } />
+
+        <Route path="/learn/:chapterSlug" element={
+          <Guard><LearnPage /></Guard>
+        } />
+        <Route path="/practice/:chapterSlug" element={
+          <Guard><PracticePage /></Guard>
+        } />
+
         <Route path="/progress" element={
-          <PrivateRoute><ProgressPage /></PrivateRoute>
+          <Guard><ProgressPage /></Guard>
         } />
-        <Route path="*" element={<NotFoundPage />} />
+
+        <Route path="*" element={<Navigate to="/" replace />} />
+
       </Routes>
     </BrowserRouter>
   )
